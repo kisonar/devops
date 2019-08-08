8d9de40f96676b0d9f21959d7ae0c2a4bb729aa5
Marcin Migdal <marcin.migdal@nokia.com>
2019-08-08 12:27:25 +0200

    reanming files to avoid problems in future

--- scripts/test.sh
+++ scripts/test.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
-FILES_ZIP=/path/to/*.zip
+FILES_ZIP=./input-files/*.zip
 for f in $FILES_ZIP
 do
   echo "Processing $f file..."
   # take action on each file. $f store current file name
-  unzip -o -d ./input-files/ $f
+  unzip -o -d ./input-files $f
   mv $file ./input-files/archive/
 done
