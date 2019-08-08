1065bf94cacebe44ad5d519ab12af0d639c11d19
Marcin Migdal <marcin.migdal@nokia.com>
2019-08-08 14:38:55 +0200

    works extracting and moving files

--- scripts/test.sh
+++ scripts/test.sh
@@ -2,22 +2,29 @@
 BASE_PATH=./input-files
 ARCHIVE_PATH=${BASE_PATH}/archive
 FILES_ZIP=${BASE_PATH}/*.zip
-value="*"
-for file in $FILES_ZIP
-do
-  echo "Processing file $file"
-  fileName=${file##*/}
-  echo "File name: $fileName"
-  pureFileName=${fileName%.*}
-  echo "pureFileName : $pureFileName"
 
-  if [[ -w $file ]]; then
-    echo "Extracting archive $fileName ..."
-    unzip -o -d ./input-files $file
-    echo "Extraction completed"
-    mv $file ${ARCHIVE_PATH}/$fileName
-    echo "Moved $file to ${ARCHIVE_PATH}/$fileName"
-  else
-    echo "File $file is not zip. Skipping..."
-  fi
-done
+extraction_function(){
+
+for file in $1
+  do
+    echo "Processing file $file"
+    fileName=${file##*/}
+    echo "File name: $fileName"
+    filePureName=${fileName%.*}
+    echo "pureFileName : $filePureName"
+    fileExtension=${fileName##*.}
+    echo "fileExtension : $fileExtension"
+
+    if [[ -w $file ]]; then
+      echo "Extracting archive $fileName ..."
+      unzip -o -d ./input-files $file
+      echo "Extraction completed"
+      mv $file ${ARCHIVE_PATH}/$fileName
+      echo "Moved $file to ${ARCHIVE_PATH}/$fileName"
+    else
+      echo "File $file is not zip nor tar. Skipping..."
+    fi
+  done
+}
+
+extraction_function $FILES_ZIP
