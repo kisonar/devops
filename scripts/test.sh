ea4d7fc05a683bdfde63613746919a84d041bcee
Marcin Migdal <marcin.migdal@nokia.com>
2019-08-08 14:17:41 +0200

    works extracting and moving files

--- scripts/test.sh
+++ scripts/test.sh
@@ -1,9 +1,23 @@
 #!/usr/bin/env bash
-FILES_ZIP=./input-files/*.zip
-for f in $FILES_ZIP
+BASE_PATH=./input-files
+ARCHIVE_PATH=${BASE_PATH}/archive
+FILES_ZIP=${BASE_PATH}/*.zip
+value="*"
+for file in $FILES_ZIP
 do
-  echo "Processing $f file..."
-  # take action on each file. $f store current file name
-  unzip -o -d ./input-files $f
-  mv $file ./input-files/archive/
+  echo "Processing file $file"
+  fileName=${file##*/}
+  echo "File name: $fileName"
+  pureFileName=${fileName%.*}
+  echo "pureFileName : $pureFileName"
+
+  if [[ -w $file ]]; then
+    echo "Extracting archive $fileName ..."
+    unzip -o -d ./input-files $file
+    echo "Extraction completed"
+    mv $file ${ARCHIVE_PATH}/$fileName
+    echo "Moved $file to ${ARCHIVE_PATH}/$fileName"
+  else
+    echo "File $file is not zip. Skipping..."
+  fi
 done
