1b7be8cb413ad2223e2dcb52050f68bca23efed7
Marcin Migdal <marcin.migdal@nokia.com>
2019-08-27 16:16:25 +0200

    Preparing for parsing XML files

--- monitoring/testNxlog.sh
+++ monitoring/testNxlog.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
 find ./nxlog/files/input ! -name 'input.log' -type f -exec rm -f {} +
 cat /dev/null > ./nxlog/files/output/out.csv
+cat /dev/null > ./nxlog/files/output/out.json
 cp -a ./nxlog/files-testing/. ./nxlog/files/input
