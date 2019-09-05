0bffa2b1844a4c55e8dd378fe3c8fcd285e4ac22
Marcin Migdal <marcin.migdal@nokia.com>
2019-09-05 19:36:49 +0200

    xml2json alomst done - only single quota at the beginning

--- scripts/python/xml2json.py
+++ scripts/python/xml2json.py
@@ -1,21 +1,30 @@
 import xmltodict
 import json
 import os
+import sys
+import pprint
 
-directory = '/home/marcin/development/GS/testing-inputs'
+#directory = '/home/importer/ftp'
+directory=sys.argv[1]
 
 for filename in os.listdir(directory):
-  print filename
   if filename.endswith(".xml"):
+    print filename
     filepathxml = os.path.join(directory, filename)
     f = open(filepathxml, 'r')
     text = f.read()
-    xpars = xmltodict.parse(text)
-    json = json.dumps(xpars)
-    print json
+    xmlContentAsDict = xmltodict.parse(text,encoding='utf-8')
+    print "XML to dickt"
+    jsoncontent = json.dumps(xmlContentAsDict, indent=1)
+    print "Generated JSON content is: "
+    print jsoncontent
+    jsoncontent = jsoncontent.replace('\n','')
 
-    continue
+    purefilename = os.path.splitext(filename)[0]
+    jsonfilename = purefilename + '.json'
+    filepathjson = os.path.join(directory, jsonfilename)
+    with open(filepathjson, 'w') as outfile:
+      pp = pprint.PrettyPrinter(indent=2, stream=outfile)
+      pp.pprint(jsoncontent)
   else:
     continue
-
-
