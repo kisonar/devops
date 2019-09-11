918734170503c3cd74947aceea5e3724dc2e0135
Marcin Migdal <marcin.migdal@nokia.com>
2019-09-11 15:25:12 +0200

    Adding Python stuff xml & yml 2 json
    Adding bash script related to python scripts
    
    Cleanup in .gitingnore

--- scripts/python/xml2json.py
+++ scripts/python/xml2json.py
@@ -2,32 +2,16 @@ import xmltodict
 import json
 import os
 import sys
-import pprint
 
-#directory = '/home/importer/ftp'
 directory=sys.argv[1]
 
 for filename in os.listdir(directory):
   if filename.endswith(".xml"):
-    print filename
-    filepathxml = os.path.join(directory, filename)
-    file = open(filepathxml, 'r')
-    xmlContentAsDict = xmltodict.parse(file.read(),encoding='utf-8')
-    jsonContent = json.dumps(xmlContentAsDict, indent=1)
-    jsonContentNoNewLines = jsonContent.replace('\n','')
-    purefilename = os.path.splitext(filename)[0]
-
-    jsonfilenamepartial = purefilename + '.json-partial'
-    jsonfilename = purefilename + '.json'
-
-    filepathjson = os.path.join(directory, jsonfilename)
-    filepathjsonpartial = os.path.join(directory, jsonfilenamepartial)
-
-    with open(filepathjsonpartial, 'wr') as outfile:
-      pp = pprint.PrettyPrinter(indent=1, stream=outfile)
-      pp.pprint(jsonContentNoNewLines)
-    with open(filepathjsonpartial) as filein, open(filepathjson,'wr') as fileout:
-      for line in filein:
-        line=line.replace("'","") # remove ' from beginning of JSON created from XML
-        fileout.write(line)
-    os.remove(filepathjsonpartial) # remove json-partail
+    file_xml_path = os.path.join(directory, filename)
+    file_pure_name = os.path.splitext(filename)[0]
+    file_json_name = file_pure_name + '.json'
+    file_json_path = os.path.join(directory, file_json_name)
+    with open(file_xml_path, 'r') as xml_in, open(file_json_path, "w+") as json_out:
+      xmlString = xml_in.read()
+      json_as_string = json.dumps(xmltodict.parse(xmlString), indent=4)
+      json_out.write(json_as_string)
