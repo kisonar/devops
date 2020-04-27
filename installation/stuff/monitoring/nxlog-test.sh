12b0d7e46fc667c321e6d4a4482969c5c3d66cf6
Marcin Migdal <marcin.migdal@nokia.com>
2020-04-28 01:04:47 +0200

    refactor
    Signed-off-by: Marcin Migdal <marcin.migdal@nokia.com>

--- installation/stuff/monitoring/nxlog-test.sh
+++ installation/stuff/monitoring/nxlog-test.sh
@@ -2,4 +2,5 @@
 find ./nxlog/files/input ! -name 'input.log' -type f -exec rm -f {} +
 cat /dev/null > ./nxlog/files/output/out.csv
 cat /dev/null > ./nxlog/files/output/out.json
-cp -a ./nxlog/files-testing-templates/. ./nxlog/files/input
+cp -a ./nxlog/files-testing-templates/txt/. ./nxlog/files/input
+cp -a ./nxlog/files-testing-templates/json/. ./nxlog/files/input
