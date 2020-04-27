a4877f022688ab7e7db8fadd7be2e4f22b51519d
Marcin Migdal <marcin.migdal@nokia.com>
2020-04-27 22:55:20 +0200

    nxlog startup fix
    Signed-off-by: Marcin Migdal <marcin.migdal@nokia.com>

--- installation/stuff/monitoring/nxlog-test.sh
+++ installation/stuff/monitoring/nxlog-test.sh
@@ -2,4 +2,4 @@
 find ./nxlog/files/input ! -name 'input.log' -type f -exec rm -f {} +
 cat /dev/null > ./nxlog/files/output/out.csv
 cat /dev/null > ./nxlog/files/output/out.json
-cp -a ./nxlog/files-testing/. ./nxlog/files/input
+cp -a ./nxlog/files-testing-templates/. ./nxlog/files/input
