e7a08b536b8229bcbb3b04570476dd5b0803574a
Marcin Migdal <marcin.migdal@nokia.com>
2020-04-27 22:59:32 +0200

    nxlog startup fix
    Signed-off-by: Marcin Migdal <marcin.migdal@nokia.com>

--- installation/stuff/monitoring/nxlog-run.sh
+++ installation/stuff/monitoring/nxlog-run.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 docker-compose down
 find ./nxlog/files/input ! -name 'input.log' -type f -exec rm -f {} +
+chmod 666 ./nxlog/files/output/out.json
+chmod 666 ./nxlog/files/output/out.csv
 docker-compose up -d nxlog
 docker-compose logs -f
