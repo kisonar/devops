290d18042bf7f7e5ecca665dc3236c20fd16ff24
kisonar <kisonar@wp.pl>
2020-09-29 22:35:23 +0200

    fix

--- installation/stuff/monitoring/mongo-run.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-docker-compose down
-find ./nxlog/files/input ! -name 'input.log' -type f -exec rm -f {} +
-docker-compose up -d mongo
-docker-compose logs -f
---
+++ installation/stuff/rabbit-mq/README.md
@@ -0,0 +1,8 @@
+# RabbitNQ
+
+
+## Web Cinsole
+ ```
+http://<HOST>:9000
+Default login is guest guest
+```
