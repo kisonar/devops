cecea551ffe956ac8b077a1a1f138d431b962eb7
Marcin Migdal <marcin.migdal@nokia.com>
2020-11-25 13:02:27 +0100

    snap enhanced support support

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,16 +1,18 @@
 resource "openstack_compute_keypair_v2" "my-cloud-key" {
   name       = "my-key"
-  public_key = ""
-  }
+  public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDLRwr14aTJt1OIMMkU7cj9ut2ZsOjc11JDEwhlrgPQMgku1nesY3Y/TmKfIohAHwD7P/fyhCMmp45+IjUt58fvFIkuJPZlG5LAXklRJYL4nHC+QWMu9VDdV/9FFCS3k8NCGATBbpc/0vC2N9d+IlW3fxEMPweKIL72wStuOPRqjXHFSN+WVTS2vcV4XLAE6Jt0sxReL+PO5zFlAGkZUCOP5+MjRvlscA9sdIAlJzo6pXIqd+o/3OHFHbajMcbPXt8J+LNEKGmMbT/6Oir+YbaQK98jYXbRMyvTjY2futWssUPQurpRA2M8bpvPG5vZcg422SUuPuJkKiPwZtgqEADx"
+}
 
 resource "openstack_compute_instance_v2" "test" {
-  name            = "VM-name"
-  image_name      = "image-name"
-  flavor_name     = "flavor"
+  name            = "VM-${count.index}"
+  image_name      = "nesc-baseimages-fedora-32-2020-07-28"
+  #flavor_name     = "dz.004-0008"
+  #flavor_name     = "dz.004-0008"
   key_pair        = "${openstack_compute_keypair_v2.my-cloud-key.name}"
   security_groups = ["default"]
+  count           = 2
 
   network {
-    name = "network-name"
+    name = "mmigdal"
   }
 }
