7c06acb6e35d17413745f383cbb88179ae43e8ef
Marcin Migdal <marcin.migdal@nokia.com>
2020-11-25 15:17:01 +0100

    works v2

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,12 +1,43 @@
-resource "openstack_compute_instance_v2" "vms" {
-  name            = "VM-${count.index}"
-  image_name      = "image-name"
-  flavor_name     = "flavor-name"
-  key_pair        = "key-pair-name"
+resource "openstack_blockstorage_volume_v2" "volume" {
+  name = "Volume-single"
+  description = "First test volume"
+  size        = 1
+}
+resource "openstack_compute_instance_v2" "instance" {
+  name            = "Instance-single"
+  image_name      = "nesc-baseimages-fedora-32-2020-07-28"
+  flavor_name     = "dz.002-0004"
+  key_pair        = "svc-testing"
   security_groups = ["default","all-allowed"]
-  count           = 2
-
   network {
-    name = "network-name"
+    name = "mmigdal"
   }
+}
+# attach single disk to single VM
+resource "openstack_compute_volume_attach_v2" "attachment" {
+  instance_id = "${openstack_compute_instance_v2.instance.id}"
+  volume_id   = "${openstack_blockstorage_volume_v2.volume.id}"
+}
+#==================================================================================================================
+resource "openstack_blockstorage_volume_v2" "volumes" {
+  count = 3
+  name  = "Volume-${count.index}"
+  size  = 1
+}
+resource "openstack_compute_instance_v2" "instances" {
+  name            = "VM-${count.index}"
+  image_name      = "nesc-baseimages-fedora-32-2020-07-28"
+  flavor_name     = "dz.002-0004"
+  key_pair        = "svc-testing"
+  security_groups = ["default","all-allowed"]
+  count           = 3
+  network {
+    name = "mmigdal"
+  }
+}
+
+resource "openstack_compute_volume_attach_v2" "attachments" {
+  count       = 3
+  instance_id = "${openstack_compute_instance_v2.instances.*.id[count.index]}"
+  volume_id   = "${openstack_blockstorage_volume_v2.volumes.*.id[count.index]}"
 }
