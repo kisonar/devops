5ed7bdf8858638f514364e10e4104fe604fd6123
kisonar <kisonar@wp.pl>
2021-12-21 17:39:53 +0100

    Terraform + VirtualBox + Fedora 35

---
+++ terraform/virtualbox/fedora/README.md
@@ -0,0 +1,29 @@
+## Image
+```  
+https://www.linuxvmimages.com/images/fedora-34/#download-links fedora/fedora
+https://www.linuxvmimages.com/images/fedora-34/
+
+https://www.osboxes.org/fedora/#fedora-34-info
+Username: osboxes
+Password: osboxes.org
+```
+
+## Manual
+```
+https://itsfoss.com/install-fedora-in-virtualbox/
+```
+
+#### Requirements
+```
+vCPU: 2
+RAM: 4GB
+DISK: 512GB
+swap : 4GB
+/boot : 1GB
+/: 510GB
+NAT
+
+Username: fedora
+Password : fedora
+(to become root, use sudo su -)
+```
--- terraform/virtualbox/fedora/main.tf
+++ terraform/virtualbox/fedora/main.tf
@@ -3,12 +3,10 @@ resource "virtualbox_vm" "node" {
   name      = format("node-%02d", count.index + 1)
   image     = "https://app.vagrantup.com/ubuntu/boxes/bionic64/versions/20180903.0.0/providers/virtualbox.box"
   cpus      = 2
-  memory    = "4012 mib"
+  memory    = "4500 mib"
 
   network_adapter {
-    type           = "nat"
+    type           = "nat" // allowed values: nat, bridged, hostonly, internal, generic.
+    //host_interface = "vboxnet1"
   }
 }
-
-//note
-// script finishes with error code but VM is up & running
