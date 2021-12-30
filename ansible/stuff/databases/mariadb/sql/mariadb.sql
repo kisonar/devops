5c54f609ca797d41b4f899408d289e944803df17
kisonar <kisonar@wp.pl>
2021-12-30 11:27:19 +0100

    Sql arming (#52)
    * SQL updates

--- ansible/stuff/databases/mariadb/sql/mariadb.sql
+++ ansible/stuff/databases/mariadb/sql/mariadb.sql
@@ -5,35 +5,70 @@ CREATE TABLE users (
     userEmail  varchar(50),
     PRIMARY KEY (userId)
 );
-commit;
+COMMIT;
 
-insert into users(userId,userName,userPassword,userEmail) values (null,'qcsAdmin','qcsAdmin','qcsAdmin@mail.com');
-insert into users(userId,userName,userPassword,userEmail) values (null,'qcsEngineer','qcsEngineer','qcsEngineer@mail.com');
-insert into users(userId,userName,userPassword,userEmail) values (null,'qcsClient','qcsClient','qcsClient@mail.com');
-commit;
+INSERT INTO users(userId,userName,userPassword,userEmail) VALUES (null,'qcsAdmin','qcsAdmin','qcsAdmin@mail.com');
+INSERT INTO users(userId,userName,userPassword,userEmail) VALUES (null,'qcsEngineer','qcsEngineer','qcsEngineer@mail.com');
+INSERT INTO users(userId,userName,userPassword,userEmail) VALUES (null,'qcsClient','qcsClient','qcsClient@mail.com');
+INSERT INTO users(userId,userName,userPassword,userEmail) VALUES (null,'Marcin','MarcinPass','marcinmigdal@wp.pl');
+COMMIT;
 
 CREATE TABLE roles (
     roleId int(11) NOT NULL AUTO_INCREMENT,
     roleName varchar(15),
     PRIMARY KEY (roleId)
 );
-commit;
+COMMIT;
 
-insert into roles(roleId,roleName) values (null,'Administrator');
-insert into roles(roleId,roleName) values (null,'Engineer');
-insert into roles(roleId,roleName) values (null,'QCS Client');
-commit;
+INSERT INTO roles(roleId,roleName) VALUES (null,'Administrator');
+INSERT INTO roles(roleId,roleName) VALUES (null,'Engineer');
+INSERT INTO roles(roleId,roleName) VALUES (null,'QCS Client');
+INSERT INTO roles(roleId,roleName) VALUES (null,'User');
+COMMIT;
 
-CREATE TABLE user_roles (
+CREATE TABLE users_roles (
 	userId int(11) NOT NULL,
 	roleId int(11) NOT NULL,
 	FOREIGN KEY (userId) REFERENCES users (userId) ON DELETE RESTRICT ON UPDATE CASCADE,
 	FOREIGN KEY (roleId) REFERENCES roles (roleId) ON DELETE RESTRICT ON UPDATE CASCADE,
 	PRIMARY KEY (userId, roleId)
 );
-commit;
+COMMIT;
 
-insert into user_roles(userId,roleId) values (1,1);
-insert into user_roles(userId,roleId) values (2,2);
-insert into user_roles(userId,roleId) values (3,3);
-commit;
+INSERT INTO users_roles(userId,roleId) VALUES (1,1);
+INSERT INTO users_roles(userId,roleId) VALUES (2,2);
+INSERT INTO users_roles(userId,roleId) VALUES (3,3);
+INSERT INTO users_roles(userId,roleId) VALUES (4,1);
+INSERT INTO users_roles(userId,roleId) VALUES (4,2);
+INSERT INTO users_roles(userId,roleId) VALUES (4,3);
+INSERT INTO users_roles(userId,roleId) VALUES (4,4);
+COMMIT;
+
+CREATE TABLE tasks (
+    taskId int(11)  NOT NULL AUTO_INCREMENT,
+    taskName varchar(15) NOT NULL,
+    taskStatus varchar(15) NOT NULL,
+    PRIMARY KEY (taskId)
+);
+COMMIT;
+
+INSERT INTO tasks(taskId,taskName,taskStatus ) VALUES (null,'Task1', 'Ongoing');
+INSERT INTO tasks(taskId,taskName,taskStatus ) VALUES (null,'Task2', 'Done');
+INSERT INTO tasks(taskId,taskName,taskStatus ) VALUES (null,'Task3', 'Skipped');
+INSERT INTO tasks(taskId,taskName,taskStatus ) VALUES (null,'Task4', 'Cancelled');
+COMMIT;
+
+CREATE TABLE users_tasks (
+    userId int(11) NOT NULL,
+    taskId int(11) NOT NULL,
+    FOREIGN KEY (userId) REFERENCES users (userId) ON DELETE RESTRICT ON UPDATE CASCADE,
+    FOREIGN KEY (taskId) REFERENCES tasks (taskId) ON DELETE RESTRICT ON UPDATE CASCADE,
+    PRIMARY KEY (userId, taskId)
+);
+COMMIT;
+
+INSERT INTO users_tasks(userId,taskId ) VALUES (4,1);
+INSERT INTO users_tasks(userId,taskId ) VALUES (3,2);
+INSERT INTO users_tasks(userId,taskId ) VALUES (2,3);
+INSERT INTO users_tasks(userId,taskId ) VALUES (4,4);
+COMMIT;
--- ansible/stuff/databases/oracle/11xe/docker/oracle-sql/creating.sql
+++ ansible/stuff/databases/oracle/11xe/docker/oracle-sql/creating.sql
@@ -1,60 +1,60 @@
 -- as system user
 CREATE USER mossad IDENTIFIED BY mossad;
 --  DEFAULT TABLESPACE mossad #TODO
-commit;
+COMMIT;
 
 --grant permissions
 GRANT CREATE SESSION TO mossad WITH ADMIN OPTION;
-grant create procedure to mossad;
-grant create sequence to mossad;
-grant create session to mossad;
-grant create table to mossad;
-grant create trigger to mossad;
-grant create type to mossad;
+GRANT CREATE procedure to mossad;
+GRANT CREATE sequence to mossad;
+GRANT CREATE session to mossad;
+GRANT CREATE table to mossad;
+GRANT CREATE trigger to mossad;
+GRANT CREATE type to mossad;
 GRANT RESOURCE to mossad;
-commit;
+COMMIT;
 
 CREATE TABLE mossad.task_status (
   id INT NOT NULL,
   val VARCHAR2(20) NOT NULL,  
   CONSTRAINT TASK_STATUS_PK PRIMARY KEY ("ID")
 ) ;
-commit;
+COMMIT;
 CREATE UNIQUE INDEX TASK_STATUS_UNIQUE_INDEX_VAL ON mossad.TASK_STATUS (VAL);
-commit;
-insert into mossad.task_status values (1,'Not started');
-insert into mossad.task_status values (2,'Ongoing');
-insert into mossad.task_status values (3,'Finished');
-insert into mossad.task_status values (4,'Paused');
-insert into mossad.task_status values (5,'Under creation');
-commit;
+COMMIT;
+INSERT INTO mossad.task_status VALUES (1,'Not started');
+INSERT INTO mossad.task_status VALUES (2,'Ongoing');
+INSERT INTO mossad.task_status VALUES (3,'Finished');
+INSERT INTO mossad.task_status VALUES (4,'Paused');
+INSERT INTO mossad.task_status VALUES (5,'Under creation');
+COMMIT;
 
 CREATE TABLE mossad.task_type (
   id INT NOT NULL,
   val VARCHAR2(20) NOT NULL, 
   CONSTRAINT task_type_PK PRIMARY KEY ("ID")
 ) ;
-commit;
-insert into mossad.task_type values (1,'Private');
-insert into mossad.task_type values (2,'Home');
-insert into mossad.task_type values (3,'Work');
-insert into mossad.task_type values (4,'Friends');
-insert into mossad.task_type values (5,'Hobby');
-insert into mossad.task_type values (6,'None');
-commit;
+COMMIT;
+INSERT INTO mossad.task_type VALUES (1,'Private');
+INSERT INTO mossad.task_type VALUES (2,'Home');
+INSERT INTO mossad.task_type VALUES (3,'Work');
+INSERT INTO mossad.task_type VALUES (4,'Friends');
+INSERT INTO mossad.task_type VALUES (5,'Hobby');
+INSERT INTO mossad.task_type VALUES (6,'None');
+COMMIT;
 
 CREATE TABLE mossad.task_priority (
   id INT NOT NULL,
   val VARCHAR2(20) NOT NULL, 
   CONSTRAINT task_priority_PK PRIMARY KEY ("ID")
 ) ;
-commit;
-insert into mossad.task_priority values (1,'High');
-insert into mossad.task_priority values (2,'Medium');
-insert into mossad.task_priority values (3,'Low');
-insert into mossad.task_priority values (4,'Not specified');
-insert into mossad.task_priority values (5,'Unknown');
-commit;
+COMMIT;
+INSERT INTO mossad.task_priority VALUES (1,'High');
+INSERT INTO mossad.task_priority VALUES (2,'Medium');
+INSERT INTO mossad.task_priority VALUES (3,'Low');
+INSERT INTO mossad.task_priority VALUES (4,'Not specified');
+INSERT INTO mossad.task_priority VALUES (5,'Unknown');
+COMMIT;
 
 CREATE TABLE mossad.mossad_user (
   id INT NOT NULL,
@@ -64,15 +64,15 @@ CREATE TABLE mossad.mossad_user (
   password VARCHAR2(20) NOT NULL,
   CONSTRAINT mossad_user_PK PRIMARY KEY ("ID")
 ) ;
-commit;
+COMMIT;
 CREATE UNIQUE INDEX mossad_user_UNIQUE_INDEX_email ON mossad.mossad_user (email);
-commit;
+COMMIT;
 --//TODO add autoincrement
-insert into mossad.mossad_user values (1,'user1name','user1surname','user1email@wp.pl','user1password');
-insert into mossad.mossad_user values (2,'user2name','user2surname','user2email@wp.pl','user2password');
-insert into mossad.mossad_user values (3,'user3name','user3surname','user3email@wp.pl','user3password');
-insert into mossad.mossad_user values (4,'user4name','user4surname','user4email@wp.pl','user4password');
-commit;
+INSERT INTO mossad.mossad_user VALUES (1,'user1name','user1surname','user1email@wp.pl','user1password');
+INSERT INTO mossad.mossad_user VALUES (2,'user2name','user2surname','user2email@wp.pl','user2password');
+INSERT INTO mossad.mossad_user VALUES (3,'user3name','user3surname','user3email@wp.pl','user3password');
+INSERT INTO mossad.mossad_user VALUES (4,'user4name','user4surname','user4email@wp.pl','user4password');
+COMMIT;
 
 CREATE TABLE mossad.mossad_task (
   id INT NOT NULL,
@@ -88,10 +88,10 @@ CREATE TABLE mossad.mossad_task (
   CONSTRAINT mossad_task_type_fk FOREIGN KEY (type) REFERENCES mossad.task_type (id),
   CONSTRAINT mossad_task_user_fk FOREIGN KEY (userId) REFERENCES mossad.mossad_user (id)
 ) ;
-commit;
+COMMIT;
 
 INSERT INTO mossad.mossad_task VALUES ('1', 'Task title', 'Task descrption', '2', '1', '3', '6');
-commit;
+COMMIT;
 
 --TODO to add to task table dodac pozniuej
 --due time
