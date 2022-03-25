d1094085d88161ec156ea79fac90675eeff22ba9
kisonar <kisonar@wp.pl>
2022-03-25 13:35:46 +0100

    Logging + delay

--- puppeteer/example.js
+++ puppeteer/example.js
@@ -1,21 +1,25 @@
 const puppeteer = require('puppeteer');
 
+function delay(time) {
+    return new Promise(function(resolve) {
+        setTimeout(resolve, time)
+    });
+}
+
 (async () => {
     const browser = await puppeteer.launch({
         headless: false
     });
     const page = await browser.newPage();
-    await page.setViewport({width: 1366, height: 768});
+    //await page.setViewport({width: 6000, height: 2000});
     await page.goto('http://localhost:8083',{ waitUntil: 'networkidle2' });
     //await page.screenshot({ path: '1-start.png' });
+    console.log('Page loaded.. ')
+    await delay(2000);
+    console.log('Delay completed - page showed.')
     await page.click('#nx-header-signin-1145-btnInnerEl')
     //await page.screenshot({ path: '2-after-login-click.png' });
-
-    page.on('dialog', async (dialog) => {
-        console.log("Cos tam mamy"+ dialog.message());
-        await dialog.dismiss();
-        await browser.close();
-    });
-
+    await delay(2000);
+    console.log('Attempt to close browser...')
     await browser.close();
 })();
