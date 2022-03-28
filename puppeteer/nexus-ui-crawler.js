aa33e16477a6fa5e9c3880015bab7e5c29224a0b
kisonar <kisonar@wp.pl>
2022-03-28 22:00:20 +0200

    Puppeteer (#56)
    Puppeteer with Nexus:
    1) loading page
    2) click sign in button
    3) popup appears
    4) closing browser
    
    Co-authored-by: Marcin Migdal <marcinmigdal@wp.pl>

---
+++ puppeteer/nexus-ui-crawler.js
@@ -0,0 +1,35 @@
+const puppeteer = require('puppeteer');
+
+const delay = require('./delay');
+
+(async () => {
+    const browser = await puppeteer.launch({
+        headless: false
+    });
+    const page = await browser.newPage();
+    await page.setViewport({width: 1024, height: 1000});
+
+    await page.goto('http://localhost:8083',{ waitUntil: 'networkidle2' });
+    console.log('Page http://localhost:8083 loading... ')
+    await page.screenshot({ path: '1-nexus-start-page.png' });
+    console.log('Page http://localhost:8083 loaded')
+    await delay(2000)
+
+    console.log('Clicking link sign in')
+    await page.click('#nx-header-signin-1145-btnInnerEl')
+    await page.screenshot({ path: '2-after-sing-in-click.png' });
+    console.log('Clicking link sign in done')
+    await delay(3000)
+
+    await page.type('#textfield-1171-inputEl', 'admin', { delay: 200});
+    await page.type('#textfield-1172-inputEl', 'Nexus2022',{ delay: 200});
+    await delay(3000)
+    await page.click('#button-1174-btnInnerEl');
+
+    await delay(10000)
+    await page.screenshot({ path: '3-after-attempt-to-log-in.png' });
+
+    console.log('Attempt to close browser...')
+    await browser.close();
+    console.log('Browser closed')
+})();
