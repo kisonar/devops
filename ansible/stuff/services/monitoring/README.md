64b99c40ee1c3854e1359107be69d99778a0b0cd
kisonar <kisonar@wp.pl>
2024-05-01 18:34:46 +0200

    Graylog

--- ansible/stuff/services/monitoring/README.md
+++ ansible/stuff/services/monitoring/README.md
@@ -6,10 +6,33 @@
 ```
 nxlog-run.sh
 ```
+### TEST
+```
+nxlog-test.sh
+it copies template files to nxlog input, which processes it to TCP GELF or output file
+```
 
 ### Graylog
 ``` 
 GET /api/system/inputs
 curl -u user_token:token -H 'Content-Type: application/json' -H 'X-Requested-By: cli' 'http://graylog.domain.com:9000/api/system/inputs'
 
+
+
+localhost:9000
+
 ```
+
+
+Graylog
+opis
+nxlog
+definiowanie inputu z nxlog
+zapychanie
+
+TODO
+streams
+extractors
+grafana
+rabbit
+
--- ansible/stuff/services/monitoring/nxlog-test.sh
+++ ansible/stuff/services/monitoring/nxlog-test.sh
@@ -2,5 +2,27 @@
 find ./nxlog/files/input ! -name 'input.log' -type f -exec rm -f {} +
 cat /dev/null > ./nxlog/files/output/out.csv
 cat /dev/null > ./nxlog/files/output/out.json
+
+#copy templates
 cp -a ./nxlog/files-testing-templates/txt/. ./nxlog/files/input
-cp -a ./nxlog/files-testing-templates/json/. ./nxlog/files/input
+
+for i in {1..32}
+do
+   echo "Welcome file $i"
+   current_date=$(date +"%Y%m%d")
+   #echo "Current date: $current_date"
+
+   for u in {1..7345}
+   do
+      #echo "Welcome $u row"
+      #current_date=$(date +"%Y%m%d")
+      #echo "Current date: $current_date"
+      echo "Sign-Poland|$current_date|SUCCESS|$RANDOM" >> ./nxlog/files-testing-templates/txt/$i.txt
+   done
+  echo "file generation completed"
+
+  cp -a ./nxlog/files-testing-templates/txt/$i.txt ./nxlog/files/input/$i.txt
+  echo "copying generated file completed"
+  sleep 1
+done
+# cp -a ./nxlog/files-testing-templates/json/. ./nxlog/files/input
