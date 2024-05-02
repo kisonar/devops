35b0a2ad6e7d6828b130ea3d134a4a10dca2c201
kisonar <kisonar@wp.pl>
2024-05-02 17:09:02 +0200

    Graylog

--- ansible/stuff/services/monitoring/nxlog-test.sh
+++ ansible/stuff/services/monitoring/nxlog-test.sh
@@ -6,13 +6,13 @@ cat /dev/null > ./nxlog/files/output/out.json
 #copy templates ready
 #cp -a ./nxlog/files-testing-templates/txt/. ./nxlog/files/input
 
-for i in {1..32}
+for i in {1..25}
 do
    echo "Welcome file $i"
    current_date=$(date +"%Y%m%d")
    #echo "Current date: $current_date"
 
-   for u in {1..7345}
+   for u in {1..73}
    do
       echo "Sign-Poland|$current_date|SUCCESS|$RANDOM" >> ./nxlog/files-testing-templates/txt/$i.txt
    done
@@ -23,4 +23,21 @@ do
   echo "copying generated file completed"
   sleep 0
 done
+
+# Add Fin entries
+for i in {1..346}
+do
+  echo "Sign-Finland|$current_date|Nightwish|$RANDOM" >> ./nxlog/files-testing-templates/txt/NP_AAE_FIN.txt
+
+done
+
+# Add GER entries
+for i in {1..456}
+do
+  echo "Sign-Germany|$current_date|Rammstein|$RANDOM" >> ./nxlog/files-testing-templates/txt/NP_AAE_GER.txt
+
+done
+
+
+
 cp -a ./nxlog/files-testing-templates/txt/. ./nxlog/files/input
